2f1dc994f35e9ff3fd2e1d30155a45f90fd45852
HimeshRoy <himeshroy.cse@gmail.com>
2026-06-11 21:19:30 +0530

    email service changed from gmail to smtp.gmail.com

--- src/services/email/email.service.ts
+++ src/services/email/email.service.ts
@@ -1,21 +1,30 @@
 import nodemailer from "nodemailer";
 
-export const transporter =
-  nodemailer.createTransport({
-    service: "gmail",
+export const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
 
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+  port: 587,
 
-export const sendEmail = async (
-  to: string,
-  subject: string,
-  html: string
-) => {
+  secure: false,
 
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+transporter.verify(
+  (error, success) => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(
+        "SMTP Ready"
+      );
+    }
+  }
+);
+
+export const sendEmail = async (to: string, subject: string, html: string) => {
   return transporter.sendMail({
     from: `"NBCA" <${process.env.EMAIL_USER}>`,
     to,
