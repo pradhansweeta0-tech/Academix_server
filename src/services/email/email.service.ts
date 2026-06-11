592d655c951f5d2160c30c39d545b1433ba68089
HimeshRoy <himeshroy.cse@gmail.com>
2026-06-11 21:47:30 +0530

    env check route added temply

--- src/services/email/email.service.ts
+++ src/services/email/email.service.ts
@@ -1,31 +1,21 @@
 import nodemailer from "nodemailer";
 
-export const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+export const transporter =
+  nodemailer.createTransport({
+    service: "gmail",
 
-  port: 587,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
 
-  secure: false,
+export const sendEmail = async (
+  to: string,
+  subject: string,
+  html: string
+) => {
 
-  family: 4, 
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
-transporter.verify(
-  (error, success) => {
-    if (error) {
-      console.error(error);
-    } else {
-      console.log(
-        "SMTP Ready"
-      );
-    }
-  }
-);
-
-export const sendEmail = async (to: string, subject: string, html: string) => {
   return transporter.sendMail({
     from: `"NBCA" <${process.env.EMAIL_USER}>`,
     to,
