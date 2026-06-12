d55ceaaabec5bf834465b052fb936b3d856c5088
HimeshRoy <himeshroy.cse@gmail.com>
2026-06-12 22:51:26 +0530

    email service changed

--- src/services/email/email.controller.ts
+++ src/services/email/email.controller.ts
@@ -2,17 +2,20 @@ import { Request, Response } from "express";
 import { sendEmail } from "../../services/email/email.service";
 
 export const testEmailController = async (req: Request, res: Response) => {
-  await sendEmail(
-    req.body.email,
-    "NBCA Test Email",
-    `
-      <h1>NBCA Email Working 🚀</h1>
-      <p>Your email service is configured correctly.</p>
-      `,
-  );
+  try {
+    await sendEmail(req.body.email, "NBCA Test Email", "<h1>Brevo Test</h1>");
 
-  res.json({
-    success: true,
-    message: "Email sent",
-  });
+    res.json({
+      success: true,
+      message: "Email sent",
+    });
+  } catch (error: any) {
+    console.error("FULL EMAIL ERROR");
+    console.error(error);
+    console.error(error?.code);
+    console.error(error?.response);
+    console.error(error?.message);
+
+    throw error;
+  }
 };
--- src/services/email/email.service.ts
+++ src/services/email/email.service.ts
@@ -1,15 +1,19 @@
 import nodemailer from "nodemailer";
 
-export const transporter =
-  nodemailer.createTransport({
-    host: "smtp-relay.brevo.com",
-    port: 587,
-    secure: false,
-    auth: {
-      user: process.env.BREVO_USER,
-      pass: process.env.BREVO_PASS,
-    },
-  });
+export const transporter = nodemailer.createTransport({
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
+
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
+
+  auth: {
+    user: process.env.BREVO_USER,
+    pass: process.env.BREVO_PASS,
+  },
+});
 
 export const sendEmail = async (
   to: string,
