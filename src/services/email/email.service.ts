8b953b32f414323203ec6d40410683a1153f798d
HimeshRoy <himeshroy.cse@gmail.com>
2026-06-12 20:51:22 +0530

    brevo added

--- src/services/email/email.service.ts
+++ src/services/email/email.service.ts
@@ -1,27 +1,25 @@
 import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
 
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: process.env.BREVO_USER,
+    pass: process.env.BREVO_PASS,
   },
 });
 
-export const sendEmail = async (to: string, subject: string, html: string) => {
-  console.log("EMAIL START");
-  console.log("TO:", to);
-
-  const result = await transporter.sendMail({
-    from: `"NBCA" <${process.env.EMAIL_USER}>`,
+export const sendEmail = async (
+  to: string,
+  subject: string,
+  html: string
+) => {
+  return transporter.sendMail({
+    from: `"NBCA" <${process.env.BREVO_USER}>`,
     to,
     subject,
     html,
   });
-
-  console.log("EMAIL SENT");
-  console.log(result);
-
-  return result;
 };
