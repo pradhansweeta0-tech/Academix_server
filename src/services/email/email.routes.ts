749d8401795d6ccccc68ab0dafe37fb436c9f9a6
HimeshRoy <himeshroy.cse@gmail.com>
2026-06-12 20:56:36 +0530

    routes chaneged for email

--- src/services/email/email.routes.ts
+++ src/services/email/email.routes.ts
@@ -4,11 +4,5 @@ import { testEmailController } from "./email.controller";
 const router = Router();
 
 router.post("/test-email", testEmailController);
-router.get("/env-check", (req, res) => {
-  res.json({
-    emailUserExists: !!process.env.EMAIL_USER,
-    emailPassExists: !!process.env.EMAIL_PASS,
-  });
-});
 
 export default router;
