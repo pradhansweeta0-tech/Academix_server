d21e35418348b24c0d350407792099b172786d65
HimeshRoy <himeshroy.cse@gmail.com>
2026-06-12 22:23:18 +0530

    email service changed

--- src/services/email/email.service.ts
+++ src/services/email/email.service.ts
@@ -1,15 +1,15 @@
 import nodemailer from "nodemailer";
 
-export const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false,
-
-  auth: {
-    user: process.env.BREVO_USER,
-    pass: process.env.BREVO_PASS,
-  },
-});
+export const transporter =
+  nodemailer.createTransport({
+    host: "smtp-relay.brevo.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.BREVO_USER,
+      pass: process.env.BREVO_PASS,
+    },
+  });
 
 export const sendEmail = async (
   to: string,
@@ -17,7 +17,7 @@ export const sendEmail = async (
   html: string
 ) => {
   return transporter.sendMail({
-    from: `"NBCA" <${process.env.BREVO_USER}>`,
+    from: '"NBCA" <info@nbca.co.in>',
     to,
     subject,
     html,
