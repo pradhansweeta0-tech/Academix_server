a701b4f7b83a6b01bade66cd3d5e2d24213fa788
HimeshRoy <himeshroy.cse@gmail.com>
2026-06-27 12:12:49 +0530

    id-card view route added

--- src/modules/profile/id-card.service.ts
+++ src/modules/profile/id-card.service.ts
@@ -69,8 +69,8 @@ export const generateStudentIdCard = async (studentId: string) => {
     // Logo
     try {
       doc.image(logoPath, 10, 5, {
-        width: 30,
-        height: 30,
+        width: 35,
+        height: 35,
       });
     } catch (error) {
       console.error("Logo load failed");
